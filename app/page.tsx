185c100d461d8f880356ad47bfda5f956f7166af
wildanmkhdev <wildankhalidun97@gmail.com>
2026-07-20 13:48:20 +0700

    add file

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>halo woi</h2>
+      <h2>halo ferdi</h2>
     </div>
   );
 }
