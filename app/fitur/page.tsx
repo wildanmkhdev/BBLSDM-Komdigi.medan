881d6a5a89628df1807dd08af25aa59cb651df5a
wildanmkhdev <wildankhalidun97@gmail.com>
2026-07-20 10:56:25 +0700

    first commit

---
+++ app/fitur/page.tsx
@@ -0,0 +1,8 @@
+
+export default function fitur() {
+    return (
+        <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+            <h2>fitur</h2>
+        </div>
+    );
+}
