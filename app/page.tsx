f6f198114c53f29c632f1d548c23dae24c065f16
alridhozikri <alridhozikri08@gmail.com>
2026-07-20 13:55:29 +0700

    add woi

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>halo ferdi kesuma</h2>
+      <h2>woiiiiiii</h2>
     </div>
   );
 }
