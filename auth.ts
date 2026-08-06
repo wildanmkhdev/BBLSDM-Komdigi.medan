cd00ffa2f73ed6232e04f0d4c7d8d691b1d5a80a
wildanmkhdev <wildankhalidun97@gmail.com>
2026-08-06 09:55:28 +0700

    chore: explicitly set secret option in NextAuth config

--- auth.ts
+++ auth.ts
@@ -10,6 +10,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
   adapter: PrismaAdapter(prisma),
   session: { strategy: "jwt" },
+  secret: process.env.AUTH_SECRET || process.env.NEXTAUTH_SECRET,
   providers: [
     Credentials({
       async authorize(credentials) {
